365f0512224e9ceba696732551c8980792351d44
Angel Fontalvo <afontalv@outlook.com>
2018-07-28 08:51:30 -0400

    updating model and results, as well as script used for testing

---
+++ test_model.py
@@ -0,0 +1,64 @@
+import numpy as np
+import os
+import tensorflow as tf
+import cv2 as cv
+from utils import label_map_util
+from utils import visualization_utils as vis_util
+
+# What model to use.
+MODEL_NAME = 'Harley_inference_graph'
+
+# Path to frozen detection graph. This is the actual model that is used for the object detection.
+PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
+
+# List of the strings that is used to add correct label for each box.
+PATH_TO_LABELS = os.path.join('training', 'object-detection.pbtxt')
+
+# Number of Objects trained
+NUM_CLASSES = 1
+
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+# Read the graph.
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+  od_graph_def = tf.GraphDef()
+  with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+    serialized_graph = fid.read()
+    od_graph_def.ParseFromString(serialized_graph)
+    tf.import_graph_def(od_graph_def, name='')
+
+with detection_graph.as_default():
+    with tf.Session(graph=detection_graph) as sess:
+        while True:
+            # Read and preprocess an image.
+            img = cv.imread('test_images/image1.jpg')
+            image_np_expanded = np.expand_dims(img, axis=0)
+            
+            image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+            boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+            scores = detection_graph.get_tensor_by_name('detection_scores:0')
+            classes = detection_graph.get_tensor_by_name('detection_classes:0')
+            num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+            # Detection from the model
+            (boxes, scores, classes, num_detections) = sess.run(
+                [boxes, scores, classes, num_detections],
+                feed_dict={image_tensor: image_np_expanded})
+
+            # Visualization of the results of a detection.
+            vis_util.visualize_boxes_and_labels_on_image_array(
+            img,
+            np.squeeze(boxes),
+            np.squeeze(classes).astype(np.int32),
+            np.squeeze(scores),
+            category_index,
+            use_normalized_coordinates=True,
+            line_thickness=8)
+
+            cv.imshow('object detection', cv.resize(img, (800,600)))
+            if cv.waitKey(25) & 0xFF == ord('q'):
+                cv.destroyAllWindows()
+                break
